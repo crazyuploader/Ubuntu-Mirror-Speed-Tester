dceeb53e86c2cd8b011d7394f87074477263d510
Jugal Kishore <me@devjugal.com>
2022-12-18 11:42:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,25 @@ from urllib.parse import urlparse
 try:
     import requests
     import jc
+    from tabulate import tabulate
 except ImportError:
     print("Please run: pipenv install")
     sys.exit(1)
 
 
+# Global Varaible(s)
+HEADER = ["Hostname", "Full URL",
+          "Average Latency (ms)", "Packet Loss (%)", "Download Speed (Bytes)", "Download Speed"]
+
+
 # Function to write CSV
-def write_to_csv(results, filename):
+def write_to_csv(results, filename, header):
     """
     Function to write CSV
     """
     with open(filename, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Hostname", "Full URL", "Average Latency (ms)",
-                        "Packet Loss (%)", "Download Speed (Bytes)", "Download Speed"])
+        writer.writerow(header)
         for result in results:
             writer.writerow(result)
 
@@ -63,29 +68,36 @@ for mirror in mirrors:
     full_url = mirror + "dists/jammy/Contents-amd64.gz"
     command = "curl -qfsLS -w '%{speed_download}' -o /dev/null --url" + " " + full_url
     curl_command = shlex.split(command)
+    ping_command = shlex.split(f"ping -c 3 {hostname}")
     try:
         # Try to run the curl command and capture the output
         curl_speed_in_bytes = subprocess.run(
             curl_command, capture_output=True, check=True).stdout.decode("utf-8")
-        if curl_speed_in_bytes == 0:
-            continue
     except subprocess.CalledProcessError:
         print(
-            f"An error occurred while running the curl command on mirror {hostname}:")
+            f"An error occurred while running the curl command on mirror {hostname}")
         continue
+    ICMP_CHECK = False
     try:
         # Try to ping the mirror and capture the output
-        ping_output = subprocess.check_output(
-            ["ping", "-c", "10", "-i", "0.25", hostname], text=True)
+        ping_output = subprocess.run(
+            ping_command, capture_output=True, check=True).stdout.decode("utf-8")
+        ICMP_CHECK = True
     except subprocess.CalledProcessError:
-        print(f"An error occurred while pinging mirror {hostname}:")
-        continue
-    parsed_ping_output = jc.parse("ping", ping_output)
-    average_ping = parsed_ping_output["round_trip_ms_avg"]
-    packet_loss = parsed_ping_output["packet_loss_percent"]
-    RESULT = [hostname, full_url, f"{average_ping} ms", f"{packet_loss}%",
-              curl_speed_in_bytes, sizeof_fmt(float(curl_speed_in_bytes))]
+        print(f"An error occurred while pinging mirror {hostname}")
+    if ICMP_CHECK is True:
+        parsed_ping_output = jc.parse("ping", ping_output)
+        AVERAGE_PING = f'{parsed_ping_output["round_trip_ms_avg"]} ms'
+        PACKET_LOSS = f'{parsed_ping_output["packet_loss_percent"]}%'
+    else:
+        AVERAGE_PING = "na"
+        PACKET_LOSS = "na"
+    RESULT = [hostname, full_url, AVERAGE_PING, PACKET_LOSS,
+        curl_speed_in_bytes, sizeof_fmt(float(curl_speed_in_bytes))]
     mirror_results.append(RESULT)
 
+
 if mirror_results:
-    write_to_csv(mirror_results, "result.csv")
+    write_to_csv(mirror_results, "result.csv", HEADER)
+    print("")
+    print(tabulate(mirror_results, HEADER))
