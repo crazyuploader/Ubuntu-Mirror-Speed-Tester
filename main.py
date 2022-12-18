d447abb37eb4979d2d18a1d8113a5282209dd516
Jugal Kishore <me@devjugal.com>
2022-12-18 10:54:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
+"""Ubuntu Mirror Speed Tester"""
 
 
-import jc
+import csv
+import sys
+from urllib.parse import urlparse
 import subprocess
 import shlex
 import requests
-from urllib.parse import urlparse
-from time import sleep
+import jc
 
 
+# Function to write CSV
+def write_to_csv(results, filename):
+    """
+    Function to write CSV
+    """
+    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Hostname", "Full URL", "Average Latency (ms)",
+                        "Packet Loss (%)", "Download Speed (Bytes)", "Download Speed"])
+        for result in results:
+            writer.writerow(result)
+
+
+# Function to return human readable size from bytes
 def sizeof_fmt(num, suffix="B/s"):
+    """
+    Function to return human readable size from bytes.
+    """
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
         if abs(num) < 1024.0:
             return f"{num:3.1f} {unit}{suffix}"
@@ -18,21 +37,23 @@ def sizeof_fmt(num, suffix="B/s"):
 
 
 # Try to retrieve the mirrors list from the specified URL
+MIRROR_URL = "http://mirrors.ubuntu.com/mirrors.txt"
 try:
-    response = requests.get("http://mirrors.ubuntu.com/mirrors.txt", timeout=10)
+    print("Getting Mirrors List from:", MIRROR_URL)
+    response = requests.get(MIRROR_URL, timeout=10)
     if response.status_code != 200:
         print("Getting mirrors list failed")
         exit(1)
     mirrors = response.text.splitlines()
+    print("Received mirrors list successfully")
+    print("")
 except requests.exceptions.RequestException as e:
     print("An error occurred while getting the mirrors list:")
     print(e)
-    exit(1)
-
-print(", ".join(["Hostname", "Full URL", "Average Latency (ms)", "Packet Loss (%)", "Download Speed (Bytes)", "Download Speed"]))
-print("")
+    sys.exit(1)
 
 # Iterate through the mirrors and perform the pings and downloads
+mirror_results = []
 for mirror in mirrors:
     hostname = urlparse(mirror).hostname
     full_url = mirror + "dists/jammy/Contents-amd64.gz"
@@ -40,22 +61,27 @@ for mirror in mirrors:
     curl_command = shlex.split(command)
     try:
         # Try to run the curl command and capture the output
-        curl_speed_in_bytes = subprocess.run(curl_command, capture_output=True).stdout.decode("utf-8")
+        curl_speed_in_bytes = subprocess.run(
+            curl_command, capture_output=True, check=True).stdout.decode("utf-8")
         if curl_speed_in_bytes == 0:
             continue
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while running the curl command on mirror {hostname}:")
-        print(e)
+    except subprocess.CalledProcessError:
+        print(
+            f"An error occurred while running the curl command on mirror {hostname}:")
         continue
     try:
         # Try to ping the mirror and capture the output
-        ping_output = subprocess.check_output(["ping", "-c", "10", "-i", "0.25", hostname], text=True)
-    except subprocess.CalledProcessError as e:
+        ping_output = subprocess.check_output(
+            ["ping", "-c", "10", "-i", "0.25", hostname], text=True)
+    except subprocess.CalledProcessError:
         print(f"An error occurred while pinging mirror {hostname}:")
-        print(e)
         continue
     parsed_ping_output = jc.parse("ping", ping_output)
     average_ping = parsed_ping_output["round_trip_ms_avg"]
     packet_loss = parsed_ping_output["packet_loss_percent"]
-    result = ", ".join([hostname, full_url, f"{average_ping} ms", f"{packet_loss}%", curl_speed_in_bytes, sizeof_fmt(float(curl_speed_in_bytes))])
-    print(result)
+    RESULT = [hostname, full_url, f"{average_ping} ms", f"{packet_loss}%",
+              curl_speed_in_bytes, sizeof_fmt(float(curl_speed_in_bytes))]
+    mirror_results.append(RESULT)
+
+if mirror_results:
+    write_to_csv(mirror_results, "result.csv")
